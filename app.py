f0a72f85de39cc1709801fd52b1d816fc2a84b17
mahi9823 <mahesh.hosing23@gmail.com>
2026-01-01 11:25:27 +0530

    Format SSH command output in hello_all functionadd

--- app.py
+++ app.py
@@ -3,8 +3,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_all():
-    return 'ssh -i "docker-in-one-shot.pem" ubuntu@ec2-15-134-205-21.ap-southeast-2.compute.amazonaws.com,sudo apt update,sudo apt -get update,sudo apt-get install docker.io,sudo systemctl status docker,docker ps,whoami,sudo usermod -aG docker $USER,newgrp docker,docker images,docker login'
+    return """
+    ssh -i "docker-in-one-shot.pem" ubuntu@ec2-15-134-205-21.ap-southeast-2.compute.amazonaws.com<br>
+    sudo apt update<br>
+    sudo apt-get update<br>
+    sudo apt-get install docker.io<br>
+    sudo systemctl status docker<br>
+    docker ps<br>
+    whoami<br>
+    sudo usermod -aG docker $USER<br>
+    newgrp docker<br>
+    docker images<br>
+    docker login
+    """
 
 @app.route('/health')
 def health():
     return 'Server is up and running'
+
